9e435ada8527a2f5bc9b2cdc543c3aa602279918
Andrés Canales <ccanalesf@ucenfotec.ac.cr>
2019-11-11 18:38:49 -0600

    Registrar cliente
    Registro en back-end funcional

--- api/server.js
+++ api/server.js
@@ -28,7 +28,7 @@ const Evento = require('./routes/evento.route');
 
 
 //Se declaran todos los accesos de los archivos routes Andres
-
+const Cliente = require('./routes/clientes.route');
 
 
 
@@ -104,7 +104,7 @@ app.use('/api', Evento);
 
 
 //Conexión a todas la rutas Andres
-
+app.use('/api', Cliente);
 
 
 
