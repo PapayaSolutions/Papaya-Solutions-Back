9613d87974a87753647ad93f39cdb51c4b1cf4f5
Francisco-Tovar <55120226+Francisco-Tovar@users.noreply.github.com>
2019-11-12 09:40:49 -0600

    Create imagen_model.js

---
+++ api/models/imagen_model.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const mongoose = require('mongoose');
+
+const imagen_schema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+        unique: false
+    },
+    URL: {
+        type: String,
+        required: true,
+        unique: true
+
+    },
+    estado: {
+        type: Boolean,
+        required: true,
+        unique: false
+    }
+
+});
+
+
+//Model, Schema en que se apoya , nombre de la coleccion en al base de datos
+module.exports = mongoose.model('Imagen', imagen_schema, 'imagenes');
