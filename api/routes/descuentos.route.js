cb8d94d9eb7607f892338e570feabc5f99d0e503
Lycos11 <marcospc094@gmail.com>
2019-11-21 22:20:44 -0600

    rutas descuentos, visualizar

---
+++ api/routes/descuentos.route.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const express = require('express'),
+    router = express.Router(),
+    Descuento = require('../models/descuento.model')
+const mongoose = require('mongoose');
+
+//registrar descuento
+router.post('/descuentos', function(req, res) {
+    let body = req.body;
+    let nuevo_descuento = new Descuento({
+        nombre: body.nombre,
+        descripcion: body.descripcion,
+        porcentaje: body.porcentaje,
+        estado: 'activo'
+    });
+
+    nuevo_descuento.save(function(err, descuentoBD) {
+        if (err) {
+            res.json({
+                resultado: false,
+                msg: 'El descuento no se pudo registrar, ocurrió el siguiente error',
+                err
+            });
+        } else {
+            res.json({
+                resultado: true,
+                descuentoBD
+            })
+        }
+    });
+});
+
+/*listar poroductos*/
+router.get('/listar_descuentos', function(req, res) {
+    Descuento.find(
+        function(err, descuentoBD) {
+            if (err) {
+                res.json({
+                    resultado: false,
+                    msg: 'No se encontraron productos',
+                    err
+                });
+            } else {
+                res.json({
+                    resultado: true,
+                    descuentos: descuentoBD
+                })
+            }
+        }
+    );
+});
+module.exports = router;
---
+++ api/routes/visualizar.route.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express'),
+    router = express.Router(),
+    Cliente = require('../models/visualizar.model')
+const mongoose = require('mongoose');
+
+router.get('/ver-perfil/:correo_cliente', function(req, res) {
+
+
+    let correo_cliente = req.params.correo_cliente;
+    console.log(correo_cliente)
+    Cliente.findOne({ correo_cliente: correo_cliente }, function(err, clienteBD) {
+        if (err) {
+            return res.json({
+                success: false,
+                msj: 'No se encontró ningún cliente con ese correo',
+                err
+
+            });
+        } else {
+            return res.json({
+                success: true,
+                cliente: clienteBD
+            });
+        }
+    })
+});
+module.exports = router;
