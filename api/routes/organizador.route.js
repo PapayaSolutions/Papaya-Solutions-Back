6f77352a35d96eccec064f1c22408c56ab89f5e7
Wilfredodiazalguera <wdiaza@ucenfotec.ac.cr>
2019-11-23 21:12:06 -0600

    Se realiza model y controlador de organizador de eventos p fisica
    Se realiza model y controlador de organizador de eventos p fisica

---
+++ api/routes/organizador.route.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const express = require('express'),
+    router = express.Router(),
+    Organizador = require('../models/organizador.model')
+const mongoose = require('mongoose');
+
+//registrar organizador
+router.post('/registrar-organizador', function(req, res) {
+    let body = req.body;
+    let nuevo_organizador = new Organizador({
+
+        nombre: body.nombre,
+        correo: body.correo,
+        edad: body.edad,
+        genero: body.genero,
+        cedula: body.cedula,
+        provincia: body.provincia,
+        canton: body.canton,
+        distrito: body.direccion,
+        direccion: body.direccion,
+        experiencia: body.experiencia,
+        estado: body.estado
+    });
+
+    nuevo_organizador.save(function(err, organizador) {
+        if (err) {
+            res.json({
+                resultado: false,
+                msg: 'El organizador no se pudo registrar, ocurrió el siguiente error',
+                err
+            });
+        } else {
+            res.json({
+                resultado: true,
+                organizador
+            })
+        }
+    });
+});
+
+/*listar poroductos*/
+router.get('/listar_organizador', function(req, res) {
+    Organizador.find(
+        function(err, organizador) {
+            if (err) {
+                res.json({
+                    resultado: false,
+                    msg: 'No se encontraron productos',
+                    err
+                });
+            } else {
+                res.json({
+                    resultado: true,
+                    descuentos: organizador
+                })
+            }
+        }
+    );
+});
+module.exports = router;
